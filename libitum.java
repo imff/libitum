8c8b0dfa803edc945b9c3580034add939c59ff7e
imff <39280853+imff@users.noreply.github.com>
2018-05-16 19:00:35 -0500

    Update libitum.java

--- libitum.java
+++ libitum.java
@@ -14,16 +14,8 @@ public class libitum {
         objEsc1.add("llave2");
         
         ArrayList<String> descEsc1 = new ArrayList<String>();
-        descEsc1.add("Bienvenido *NOMBRE DEL ROBOT*. \nHas despertado después de 4 años, te encuentras en un almacén que está completamente oscuro, recuerda que cuentas con visión nocturna");
-        descEsc1.add("¡Bien! Ahora puedes observar... A la derecha hay varias cosas que puedes explorar, pero a la izquierda hay una puerta."); // En caso de que antes de inpeccionar vaya directamente a la puerta, se deberá considerar como acción inválida ya quela puerta está cerrada
-        
-        //descEsc1.add("¡Hey! JAJA ¿En verdad creíste que sería tan fácil salir de aquí? Está cerrada.\n ");
-        
-        descEsc1.add("Por la derecha hay un montón de robots colgados casi igual de destartalados que tú y frente a ellos hay un escritorio."):
-        descEsc1.add("El escritorio tiene un cajón");
-        descEsc1.add("Dentro de él hay varios documentos y dos llaves... ¡Hay una llave con tu nombre!"); //Podríamos recomendarle que guarde la llave?
-        descEsc1.add("Parece que no hay algo más para observar"); //Debería validar que ahora vaya hacia la puerta y salga
-        
+        descEsc1.add("Bienvenido *NOMBRE DEL ROBOT*. \nHas despertado después de 4 años, te encuentras en un almacén.");
+        descEsc1.add("Por la izquierda hay un escritorio y a la derecha, en la parte de abajo hay unas escaleras."); 
 
         escenario escenario1= new escenario(
             1, "Almacen", objEsc1, descEsc1
